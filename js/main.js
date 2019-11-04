14954d71b78d4799e66eb3036f431d04ed5d8994
Will <wgannon@wisc.edu>
2019-11-04 15:51:50 -0500

    Multiple Layer CSS
    Pulled multiple layers from carto and am able to use CSS from carto. So may be able to eventually add in IMG classifications on the Carto JS end

--- js/main.js
+++ js/main.js
@@ -16,49 +16,58 @@ L.tileLayer('https://tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey=6f2
 //connect to dataset
 //call carto client
 var client = new carto.Client({
-    apiKey: 'PUzc3fQC_OgJS8Ppcp1h2Q',
+    apiKey: 'hOoKOiroadj3UzrLaXpPWQ',
     username: 'wgannon42'
 });
 
-
+ 
 // returns the version of the library
-const source = new carto.source.Dataset('allairestatepark');
-console.log(source);
-const style = new carto.style.CartoCSS(`
-        #layer{
-          marker-width: 7;
-          marker-fill: #EE4D5A;
-          marker-fill-opacity: 0.9;
-          marker-line-color: #FFFFFF;
-          marker-line-width: 1;
-          marker-line-opacity: 1;
-          marker-type: ellipse;
-          marker-allow-overlap: true;
-            }
-        #layer {
-	  line-width: 2.5;
-	  line-color: ramp([trail_name], (#7F3C8D, #11A579, #3969AC, #F2B701, #E73F74, #80BA5A, #E68310, #008695, #A5AA99), ("Boy Scout Trail", "Brisbane Trail", "Canal Trail", "Capital to Coast Connector", "Mountain Laurel Trail", "Nature Trail", "Pine Trail", "Upper Squankum Trail"), "=");
-	  line-comp-op: overlay;
-	}
-	#layer::labels {
-	  text-name: [trail_name];
-	  text-face-name: 'DejaVu Sans Book';
-	  text-size: 10;
-	  text-fill: #FFFFFF;
-	  text-label-position-tolerance: 0;
-	  text-halo-radius: 1;
-	  text-halo-fill: #6F808D;
-	  text-dy: -10;
-	  text-allow-overlap: true;
-	  text-placement: line;
-	  text-placement-type: dummy;
-	}
+const trails_source = new carto.source.Dataset('allaire_trails');
+console.log(trails_source);
+const trails_style = new carto.style.CartoCSS(`
+   #layer {
+          line-width: 2.5;
+          line-color: ramp([trail_name], (#7F3C8D, #11A579, #3969AC, #F2B701, #E73F74, #80BA5A, #E68310, #008695, #A5AA99), ("Boy Scout Trail", "Brisbane Trail", "Canal Trail", "Capital to Coast Connector", "Mountain Laurel Trail", "Nature Trail", "Pine Trail", "Upper Squankum Trail"), "=");
+          line-comp-op: overlay;
+            ::labels {
+              text-name: [trail_name];
+              text-face-name: 'DejaVu Sans Book';
+              text-size: 10;
+              text-fill: #FFFFFF;
+              text-label-position-tolerance: 0;
+              text-halo-radius: 1;
+              text-halo-fill: #6F808D;
+              text-dy: -10;
+              text-allow-overlap: true;
+              text-placement: line;
+              text-placement-type: dummy;
+                }
 
+            }`);
+
+
+
+const points_source = new carto.source.Dataset('allaire_points');
+console.log(points_source);
+const points_style = new carto.style.CartoCSS(`
+   #layer {
+  marker-width: 7;
+  marker-fill: ramp([feature_ty], (#855C75, #D9AF6B, #AF6458, #736F4C, #526A83, #625377, #68855C, #9C9C5E, #A06177, #8C785D, #7C7C7C), ("Historic Site", "Parking", "Fishing", "Restroom", "Entrance", "Group Campground", "Office", "Picnic Pavilion", "Campground", "Concession"), "=");
+  marker-fill-opacity: 1;
+  marker-allow-overlap: true;
+  marker-line-width: 1;
+  marker-line-color: #FFFFFF;
+  marker-line-opacity: 1;
+    }
       `);
-const layer = new carto.layer.Layer(source, style);
+
+const trails_layer = new carto.layer.Layer(trails_source, trails_style);
+console.log(trails_layer);
+const points_layer = new carto.layer.Layer(points_source, points_style);
+console.log(points_layer);
 
 
-client.addLayer(layer, style);
+client.addLayers([trails_layer, points_layer]);
 client.getLeafletLayer().addTo(map);
 console.log(carto.version);
 console.log(map);
